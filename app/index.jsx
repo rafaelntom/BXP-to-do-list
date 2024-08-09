01748c37f861770ef5fcb26efdb5f9cc11dadef9
RafaelTomazini <rafaelnt83@gmail.com>
2024-08-09 18:55:54 -0300

    feature: boilerplate code removed | home screen | tailwind

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import "../global.css";
+
+const HomeScreen = () => {
+  return (
+    <View className="bg-teal-600">
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
