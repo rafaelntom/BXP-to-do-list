b273c36adb111a7119043fd569749c60d1643a4a
RafaelTomazini <rafaelnt83@gmail.com>
2024-08-09 20:21:54 -0300

    feature: can add new task based on text input

--- app/index.jsx
+++ app/index.jsx
@@ -1,32 +1,49 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import "../global.css";
 
 const HomeScreen = () => {
-  const [tasks, setTasks] = useState([
-    { id: "1", text: "Task 1" },
-    { id: "2", text: "Task 2" },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  const [inputText, setInputText] = useState("");
 
   const addTask = () => {
     setTasks([
       ...tasks,
-      { id: (tasks.length + 1).toString(), text: `Task ${tasks.length + 1}` },
+      { id: (tasks.length + 1).toString(), text: inputText },
     ]);
   };
 
   return (
     <View className="flex-1 bg-pj-raisin-black p-4">
       <StatusBar backgroundColor="hsla(236, 18%, 19%, 1)" />
-      <Text className="text-pj-silver text-2xl font-bold mb-4">
+      <Text className="text-pj-silver text-2xl font-bold mb-4 text-center font-mono">
         BXP - Todo List
       </Text>
+
+      <Text className="text-white font-bold">Create a new task:</Text>
+
+      <TextInput
+        placeholder="insert new task"
+        className="bg-neutral-900 placeholder:text-pj-silver text-white my-4 rounded-xl pl-2"
+        value={inputText}
+        onChangeText={setInputText}
+      ></TextInput>
 
       <FlatList
         data={tasks}
         renderItem={({ item }) => (
-          <View className="bg-pj-cinereous p-4 mb-2 rounded-lg">
-            <Text className="text-white font-bold text-lg">{item.text}</Text>
+          <View className="bg-neutral-700 p-4 mb-2 rounded-lg">
+            <Text className="text-pj-silver font-bold text-lg">
+              {item.text}
+            </Text>
           </View>
         )}
         keyExtractor={(item) => item.id}
