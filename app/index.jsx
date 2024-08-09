6633f987710aa5624c3749a45ccb23901c0a8c63
RafaelTomazini <rafaelnt83@gmail.com>
2024-08-09 19:05:17 -0300

    feature: simple design, not renderind content dynamicly or persisting data

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import "../global.css";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
 
 const HomeScreen = () => {
+  const [tasks, setTasks] = useState([
+    { id: "1", text: "Task 1" },
+    { id: "2", text: "Task 2" },
+  ]);
+
+  const addTask = () => {
+    setTasks([
+      ...tasks,
+      { id: (tasks.length + 1).toString(), text: `Task ${tasks.length + 1}` },
+    ]);
+  };
+
   return (
-    <View className="bg-teal-600">
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-pj-raisin-black p-4">
+      <StatusBar backgroundColor="hsla(236, 18%, 19%, 1)" />
+      <Text className="text-pj-silver text-2xl font-bold mb-4">
+        BXP - Todo List
+      </Text>
+
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <View className="bg-pj-cinereous p-4 mb-2 rounded-lg">
+            <Text className="text-white font-bold text-lg">{item.text}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+
+      <TouchableOpacity
+        className="bg-pj-steel-blue p-4 rounded-lg mt-4 items-center"
+        onPress={addTask}
+      >
+        <Text className="text-pj-silver text-lg font-bold">Add Task</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({});
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pj-raisin-black": "hsla(236, 18%, 19%, 1)",
+        "pj-silver": "hsla(34, 15%, 73%, 1)",
+        "pj-cinereous": "hsla(28, 13%, 60%, 1)",
+        "pj-redwood": "hsla(14, 30%, 43%, 1)",
+        "pj-steel-blue": "hsla(209, 58%, 49%, 1)",
+      },
+    },
   },
   plugins: [],
 };
